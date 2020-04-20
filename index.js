f3f74b52da045125be99b4e646a5fe81b3589a3f
DadoLinux <ziocleto@gmail.com>
2020-04-20 11:55:12 +0100

    1.0.2

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const lastPathElement = (url) => {
   return url.substring(url.lastIndexOf('/') + 1);
 };
 
+const secondToLastPathElement = (url) => {
+  return url.substring(url.lastIndexOf('/',url.lastIndexOf('/')-1) + 1);
+};
+
 module.exports = {
-  lastPathElement
+  lastPathElement,
+  secondToLastPathElement
 };
